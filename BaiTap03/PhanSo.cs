bd46f70d18955b6f3196ee78310a57eb02b5c71d
mktl04 <154105946+mktl04@users.noreply.github.com>
2023-12-29 11:34:41 +0700

    Add files via upload

---
+++ BaiTap03/PhanSo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap03
+{
+    class PhanSo
+    {
+        private int tuso;
+        private int mauso;
+        public PhanSo()
+        {
+            tuso = 1;
+            mauso = 1;
+        }
+        public PhanSo(int tuso, int mauso)
+        {
+            this.tuso = tuso;
+            this.mauso = mauso;
+        }
+        public void setTuso(int tuso)
+        {
+            this.tuso = tuso;
+        }
+        public int getTuso()
+        {
+            return tuso;
+        }
+        public void setMauso(int mauso)
+        {
+            this.mauso = mauso;
+        }
+        public int getMauso()
+        {
+            return mauso;
+        }
+        public void toiGian()
+        {
+            int x = Math.Abs(tuso);
+            int y = Math.Abs(mauso);
+            while (x != y)
+            {
+                if (x > y)
+                    x = x - y;
+                else
+                    y = y - x;
+            }
+            tuso = tuso / x;
+            mauso = mauso / y;
+        }
+        public PhanSo cong(PhanSo ps)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = tuso * ps.mauso + ps.tuso * mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public PhanSo tru(PhanSo ps)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = tuso * ps.mauso - ps.tuso * mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public PhanSo nhan(PhanSo ps)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = tuso * ps.tuso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public PhanSo chia(PhanSo ps)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tuso = tuso * ps.mauso;
+            kq.mauso = mauso * ps.tuso;
+            return kq;
+        }
+    }
+}
---
+++ BaiTap03/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap03
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cau a:
+            PhanSo p1 = new PhanSo();
+            Console.WriteLine("Nhap thong tin phan so thu nhat:");
+            Console.Write("Nhap tu so:");
+            int tuso = int.Parse(Console.ReadLine());
+            Console.Write("Nhap mau so:");
+            int mauso = int.Parse(Console.ReadLine());
+
+            p1.setTuso(tuso);
+            p1.setMauso(mauso);
+
+            Console.WriteLine("Nhap thong tin phan so thu hai:");
+            Console.Write("Nhap tu so:");
+            tuso = int.Parse(Console.ReadLine());
+            Console.Write("Nhap mau so:");
+            mauso = int.Parse(Console.ReadLine());
+            PhanSo p2 = new PhanSo();
+            p2.toiGian();
+            PhanSo tong = p1.cong(p2);
+
+
+            Console.WriteLine("Tong hai phan so PS1[{0}/{1}], PS2[{2}/{3}] = KQ[{4}/{5}]",
+                p1.getTuso(), p1.getMauso(), p2.getTuso(), p2.getMauso(), tong.getTuso(), tong.getMauso());
+
+            Console.ReadLine();
+            //Cau b:
+            Console.WriteLine("Tinh toan day phan so:");
+            Console.Write("Nhap vao so nguyen duong:");
+            int n = int.Parse(Console.ReadLine());
+
+            PhanSo s = new PhanSo(0, 1);
+            for (int i = 1; i <= n; i++)
+            {
+                s = s.cong(new PhanSo(1, i));
+            }
+            Console.WriteLine("Ket qua day phan so: 1 + 1/2 + 1/3 + ... + 1/n = {0}/{1}", s.getTuso(),s.getMauso());
+            Console.ReadLine();
+        }
+    }
+}
